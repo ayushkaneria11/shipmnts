0b4c723157001588b91cb8a88cc7421a7ca8efba
ayushkaneria11 <kaneriaayush@gmail.com>
2021-11-28 19:20:10 +0530

    User Mutation & Query

--- StackOverflow/schema.py
+++ StackOverflow/schema.py
@@ -1,13 +1,14 @@
 import graphene
 
 import api.schema as schema
+import api.schema_user as schema_user
 
 
 
-class Query(schema.Query, graphene.ObjectType):
+class Query(schema_user.Query,schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(schema.Mutation, graphene.ObjectType):
+class Mutation(schema_user.Mutation,schema.Mutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
